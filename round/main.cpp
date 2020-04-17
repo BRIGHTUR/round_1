a20ad04ef45189117f704d21051fdc06279fb260
BRIGHTUR <50517987+BRIGHTUR@users.noreply.github.com>
2020-04-17 16:55:02 +0800

    颜色

--- round/main.cpp
+++ round/main.cpp
@@ -4,21 +4,24 @@ using namespace std;
 using namespace cv;
 int main()
 {
-	Mat src = imread("rim.png", 0);
+	Mat src = imread("rim.png");
 	Mat dist;
+	Mat dist1;
 	Mat proc;
 	Mat proc1;
 	Mat labelmat;
 	Mat statsmat;
 	Mat centermat;
 	Mat dstImage;
-	threshold(src, dist, 125, 255, THRESH_BINARY_INV);
+	cvtColor(src, dist, CV_BGR2GRAY);
+	//cvCvtColor(src, dist, CV_BGR2GRAY);
+	threshold(dist, dist1, 125, 255, THRESH_BINARY_INV);
 	//int nComp = connectedComponentsWithStats(dist, labelmat, statsmat, centermat, 8, CV_32S);
 	//cout << "圆的个数为:" << nComp - 1 << endl;
 	//imshow("ss", proc1);
 	vector<vector<Point>> contours;
 	vector<Vec4i> hierarchy;
-	findContours(dist, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+	findContours(dist1, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
 //	RotatedRect rbox = minAreaRect(contours);
 	for (int i = 0; i < contours.size(); i++)
 	{
